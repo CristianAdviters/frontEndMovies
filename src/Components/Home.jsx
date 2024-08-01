7343b5e040b3a7100e1d4c6c3d29b6f8ad93805c
cristianadviters <capacitaciones06.11@adviters.com>
2024-08-01 10:59:51 -0300

    search button to home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,12 +3,15 @@ import Nav from "./Nav";
 import Movies from "./Movies";
 import StaticMovies from "./StaticMovies";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>Welcome to Adviters Movies!</h1>
-      <button>Search Movies</button>
+      <Link to={`/search-movie`}>
+        <button>Search Movies</button>
+      </Link>
       <Movies />
       <StaticMovies />
     </>
