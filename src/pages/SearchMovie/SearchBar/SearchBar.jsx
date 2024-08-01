286cf294ed465c67e1b0d78e9c045fe090203a18
cristianadviters <capacitaciones06.11@adviters.com>
2024-08-01 15:28:10 -0300

    edit components

--- src/pages/SearchMovie/SearchBar/SearchBar.jsx
+++ src/pages/SearchMovie/SearchBar/SearchBar.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./searchBar.css";
 
-export const SearchBar = ({ searchTerm, setSearchTerm}) => {
-    return (
-        <div>
-          <input
-            type="text"
-            placeholder="Search movies..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-        </div>
-      );
-}
+export const SearchBar = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div>
+      <input
+        type="text"
+        className="sarchBar"
+        placeholder="Search movies..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </div>
+  );
+};
